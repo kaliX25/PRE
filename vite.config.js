60f45657d8a6ef0553213a586130dddd43e96200
kaliX25 <marcomarcello4m@gmail.com>
2024-06-10 13:52:48 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PRE/',
   server: {
     host: '0.0.0.0', // Allows access from the network
     port: 5173, // Ensure this is the correct port
   },
   plugins: [react()],
-  base: '/vite-react-deploy/',
 });
