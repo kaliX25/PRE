e428291f6f1d2c1d9383499bbc037653b4b16f23
kaliX25 <marcomarcello4m@gmail.com>
2024-06-10 14:19:21 +0700

    third changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PRE/',
+  // base: '/PRE/',
   server: {
     host: '0.0.0.0', // Allows access from the network
     port: 5173, // Ensure this is the correct port
